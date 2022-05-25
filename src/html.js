6866d1b7d308d7c03d2fcd4edee08042c24be538
Julian <ojtinjacar@unal.edu.co>
2022-05-25 13:49:52 -0500

    brush working

--- src/html.js
+++ src/html.js
@@ -7,11 +7,8 @@ import React from 'react'; export default function HTML(props) { return (
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> {props.headComponents} {/* Appzi: Capture Insightful Feedback */} {/*
     <script*/} {/* async*/} {/* src="https://w.appzi.io/bootstrap/bundle.js?token=iIhbb" */} {/*/>*/} {/* End Appzi */}
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/addons/p5.sound.min.js"></script>
-    <script src='https://cdn.jsdelivr.net/gh/VisualComputing/p5.treegl/p5.treegl.min.js'></script>
-    <script src="https://freshfork.github.io/p5.EasyCam/p5.easycam.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/addons/p5.sound.min.js"></script>
     <div dangerouslySetInnerHTML={{ __html: `<link rel="preload" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css" as="style" onload="this.rel=' stylesheet '" onerror="this.rel=null" />
     <link rel="preload" href="https://rsms.me/inter/inter.css" as="style" onload="this.rel='stylesheet '" onerror="this.rel=null" />`, }} /> {/*
     <link*/} {/* rel="preload" */} {/* href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css" */} {/* as="style" */} {/* onLoad="this.onload=null;this.rel='stylesheet '" */} {/*/>*/} {/*
