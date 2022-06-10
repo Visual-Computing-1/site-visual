fe43a56c555c05a806c6e33bd573d28733f41797
Julian <ojtinjacar@unal.edu.co>
2022-06-10 14:42:02 -0500

    fix color-blindness filter

--- src/components/markdown/Processing.tsx
+++ src/components/markdown/Processing.tsx
@@ -13,8 +13,45 @@ const ReactP5Wrapper = React.lazy(() =>
 
 function sketch(p5) {
   const CVDMatrix = {
+    Normal: [
+      1.0, 0.0, 0.0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+      0, 1,
+    ],
+    Protanopia: [
+      0.567, 0.433, 0.0, 0.0, 0.0, 0.558, 0.442, 0.0, 0.0, 0.0, 0.0, 0.242,
+      0.758, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+    ],
+    Protanomaly: [
+      0.817, 0.183, 0, 0, 0, 0.333, 0.667, 0, 0, 0, 0, 0.125, 0.875, 0, 0, 0, 0,
+      0, 1, 0, 0, 0, 0, 0, 1,
+    ],
+    Deuteranopia: [
+      0.625, 0.375, 0, 0, 0, 0.7, 0.3, 0, 0, 0, 0, 0.3, 0.7, 0, 0, 0, 0, 0, 1,
+      0, 0, 0, 0, 0, 1,
+    ],
+    Deuteranomaly: [
+      0.8, 0.2, 0, 0, 0, 0.258, 0.742, 0, 0, 0, 0, 0.142, 0.858, 0, 0, 0, 0, 0,
+      1, 0, 0, 0, 0, 0, 1,
+    ],
+    Tritanopia: [
+      0.95, 0.05, 0, 0, 0, 0, 0.433, 0.567, 0, 0, 0, 0.475, 0.525, 0, 0, 0, 0,
+      0, 1, 0, 0, 0, 0, 0, 1,
+    ],
+    Tritanomaly: [
+      0.967, 0.033, 0, 0, 0, 0, 0.733, 0.267, 0, 0, 0, 0.183, 0.817, 0, 0, 0, 0,
+      0, 1, 0, 0, 0, 0, 0, 1,
+    ],
+    Achromatopsia: [
+      0.299, 0.587, 0.114, 0, 0, 0.299, 0.587, 0.114, 0, 0, 0.299, 0.587, 0.114,
+      0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+    ],
+    Achromatomaly: [
+      0.618, 0.32, 0.062, 0, 0, 0.163, 0.775, 0.062, 0, 0, 0.163, 0.32, 0.516,
+      0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    ],
+
     // Color Vision Deficiency
-    Protanope: [
+    /*Protanope: [
       // reds are greatly reduced (1% men)
       0.0, 2.02344, -2.52581, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
     ],
@@ -25,19 +62,19 @@ function sketch(p5) {
     Tritanope: [
       // blues are greatly reduced (0.003% population)
       1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -0.395913, 0.801109, 0.0,
-    ],
+    ],*/
   };
   const CVDMatrixCorrection = {
     // Color Vision Deficiency
-    Protanope: [
+    Protanopia: [
       // reds are greatly reduced (1% men)
       0.0, 0.0, 0.0, 0.7, 1.0, 0.0, 0.7, 0.0, 1.0,
     ],
-    Deuteranope: [
+    Deuteranopia: [
       // greens are greatly reduced (1% men)
       1.0, 0.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.7, 1.0,
     ],
-    Tritanope: [
+    Tritanopia: [
       // blues are greatly reduced (0.003% population)
       1.0, 0.0, 0.7, 0.0, 1.0, 0.7, 0.0, 0.0, 0.0,
     ],
@@ -51,33 +88,40 @@ function sketch(p5) {
     CVDMatrixCorrection
   ) {
     img.loadPixels(); // loads image
+
     for (var id = 0; id < img.width * img.height * 4; id += 4) {
-      var L, M, S, l, m, s, R, G, B;
+      var L, M, S, l, m, s, R, G, B, A, RR, GG, BB;
+
       const r = img.pixels[id];
       const g = img.pixels[id + 1];
       const b = img.pixels[id + 2];
-      L = 17.8824 * r + 43.5161 * g + 4.11935 * b;
-      M = 3.45565 * r + 27.1551 * g + 3.86714 * b;
-      S = 0.02999566 * r + 0.184309 * g + 1.46709 * b;
-      // Simulate color blindness
-      l =
-        CVDMatrix[typeblindness][0] * L +
-        CVDMatrix[typeblindness][1] * M +
-        CVDMatrix[typeblindness][2] * S;
-      m =
-        CVDMatrix[typeblindness][3] * L +
-        CVDMatrix[typeblindness][4] * M +
-        CVDMatrix[typeblindness][5] * S;
-      s =
-        CVDMatrix[typeblindness][6] * L +
-        CVDMatrix[typeblindness][7] * M +
-        CVDMatrix[typeblindness][8] * S;
-      R = 0.0809444479 * l + -0.130504409 * m + 0.116721066 * s;
-      G = -0.0102485335 * l + 0.0540193266 * m + -0.113614708 * s;
-      B = -0.000365296938 * l + -0.00412161469 * m + 0.693511405 * s;
-      // Shift colors towards visible spectrum (apply error modification)
-      /*if (typeblindness != "Deuteranope") {
-      R = r - R;
+      const a = img.pixels[id + 3];
+      R =
+        CVDMatrix[typeblindness][0] * img.pixels[id] +
+        CVDMatrix[typeblindness][1] * img.pixels[id + 1] +
+        CVDMatrix[typeblindness][2] * img.pixels[id + 2] +
+        CVDMatrix[typeblindness][3] * img.pixels[id + 3] +
+        CVDMatrix[typeblindness][4];
+      G =
+        CVDMatrix[typeblindness][5] * img.pixels[id] +
+        CVDMatrix[typeblindness][6] * img.pixels[id + 1] +
+        CVDMatrix[typeblindness][7] * img.pixels[id + 2] +
+        CVDMatrix[typeblindness][8] * img.pixels[id + 3] +
+        CVDMatrix[typeblindness][9];
+      B =
+        CVDMatrix[typeblindness][10] * img.pixels[id] +
+        CVDMatrix[typeblindness][11] * img.pixels[id + 1] +
+        CVDMatrix[typeblindness][12] * img.pixels[id + 2] +
+        CVDMatrix[typeblindness][13] * img.pixels[id + 3] +
+        CVDMatrix[typeblindness][14];
+      A =
+        CVDMatrix[typeblindness][15] * img.pixels[id] +
+        CVDMatrix[typeblindness][16] * img.pixels[id + 1] +
+        CVDMatrix[typeblindness][17] * img.pixels[id + 2] +
+        CVDMatrix[typeblindness][18] * img.pixels[id + 3] +
+        CVDMatrix[typeblindness][19];
+
+      /*R = r - R;
       G = g - G;
       B = b - B;
       RR = (CVDMatrixCorrection[correctionerror][0] * R) + (CVDMatrixCorrection[correctionerror][1] * G) + (CVDMatrixCorrection[correctionerror][2] * B);
@@ -86,17 +130,20 @@ function sketch(p5) {
       // Add compensation to original values
       R = RR + r;
       G = GG + g;
-      B = BB + b;
-      }*/
+      B = BB + b;*/
+
       if (R < 0) R = 0;
       if (R > 255) R = 255;
       if (G < 0) G = 0;
       if (G > 255) G = 255;
       if (B < 0) B = 0;
       if (B > 255) B = 255;
+      if (A < 0) B = 0;
+      if (A > 255) B = 255;
       img.pixels[id] = R; // Red
       img.pixels[id + 1] = G; // Green
       img.pixels[id + 2] = B; // Blue
+      img.pixels[id + 3] = A; // Alpha
     }
     img.updatePixels();
     return img;
@@ -115,24 +162,24 @@ function sketch(p5) {
     p5.image(z, 15, 0, z.width, z.height);
     imgPro = blindnessfilter(
       z,
-      'Protanope',
-      'Protanope',
+      'Protanopia',
+      'Protanopia',
       CVDMatrix,
       CVDMatrixCorrection
     );
     p5.image(imgPro, x.width + 25, 0, imgPro.width, imgPro.height);
     imgDeu = blindnessfilter(
       x,
-      'Deuteranope',
-      'Protanope',
+      'Deuteranopia',
+      'Deuteranopia',
       CVDMatrix,
       CVDMatrixCorrection
     );
     p5.image(imgDeu, x.width * 2 + 45, 0, imgDeu.width, imgDeu.height);
     imgTri = blindnessfilter(
       y,
-      'Tritanope',
-      'Protanope',
+      'Tritanopia',
+      'Tritanopia',
       CVDMatrix,
       CVDMatrixCorrection
     );
@@ -142,8 +189,8 @@ function sketch(p5) {
     p5.image(cards, 0, imgTri.height + 50, cards.width, cards.height);
     imgCards = blindnessfilter(
       cards,
-      'Protanope',
-      'Protanope',
+      'Tritanopia',
+      'Tritanopia',
       CVDMatrix,
       CVDMatrixCorrection
     );
