258111bcaa870cad40692250f561e44ebaae5e40
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-06-18 14:45:19 -0500

    Updates

--- js/proceduralTexturing/sketch.js
+++ js/proceduralTexturing/sketch.js
@@ -1,57 +1,21 @@
-/*const ProceduralTexturing = (sketch) => {
-    let sh, u_time;
-    const SIDE = 700;
-    const PADDING = SIDE / 4;
-
-    let u_seed;
-
-    sketch.preload = () => {
-        sh = sketch.readShader("ProceduralTexturing.frag")
-    }
-
-    sketch.setup = () => {
-        sketch.createCanvas(SIDE, SIDE, sketch.WEBGL)
-        u_time = 100;
-        u_seed = sketch.random(1, 30);
-    }
-
-    sketch.draw = () => {
-        sketch.background(0);
-        sketch.shader(sh);
-        
-        sh.setUniform("u_time", u_time);
-        sh.setUniform("u_resolution", [sketch.width, sketch.height]);
-        sh.setUniform("u_seed", u_seed);
-        
-        sketch.rect(
-            -sketch.width + PADDING,
-            -sketch.height + PADDING,
-            sketch.width + PADDING,
-            sketch.height + PADDING
-        );
-    }
-}
-
-let p5ProceduralTexturing = new p5(ProceduralTexturing, document.getElementById('ProceduralTexturing'));
-*/
-
 let sh, u_time;
+let button;
 
-const SIDE = 450;
+const SIDE = 430;
 const PADDING = SIDE / 4;
 
 let u_seed;
 
 function preload(){
-  sh = readShader("http://localhost:8000/js/proceduralTexturing/ProceduralTexturing.frag")
+  sh = readShader("https://visual-computing-1.github.io/visual-site/js/proceduralTexturing/ProceduralTexturing.frag")
 }
 
 function setup() {
   createCanvas(SIDE, SIDE, WEBGL)
   u_time = createSlider(95, 100, 96, 1);
   u_time.position(10, 25);
-  // u_time.style('width', '280px');
-
+  button = createButton("randomly generate");
+  button.mousePressed(changeBG);
   u_seed = random(1, 30);
 }
 
@@ -64,4 +28,8 @@ function draw() {
   sh.setUniform("u_seed", u_seed);
   
   rect(-width + PADDING, -height + PADDING, width + PADDING, height + PADDING);
+}
+
+function changeBG() {
+  u_seed = random(1, 30);
 }
